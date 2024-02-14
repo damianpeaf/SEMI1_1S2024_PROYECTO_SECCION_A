d84cfddd1f3878cb309c5d6809adef3d303653bd
danielcuque <3024465830102@ingenieria.usac.edu.gt>
2024-02-13 20:39:10 -0600

    feature: install aws skd

--- backend-node/src/app.module.ts
+++ backend-node/src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { FilesService } from './files/files.service';
+import { FilesModule } from './files/files.module';
+import { ConfigModule } from '@nestjs/config';
+
+import { envConfiguration } from './config/app.config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    AuthModule
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [envConfiguration],
+    }),
+    AuthModule,
+    FilesModule,
   ],
+  providers: [FilesService],
 })
 export class AppModule { }
---
+++ backend-node/src/config/app.config.ts
@@ -0,0 +1,5 @@
+export const envConfiguration = () => ({
+    AWS_S3_REGION: process.env.AWS_S3_REGION,
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS: process.env.AWS_SECRET_ACCESS,
+})
