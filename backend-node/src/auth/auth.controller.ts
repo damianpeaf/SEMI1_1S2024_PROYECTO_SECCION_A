d90baadb6a29181ab30000287719f76bf482482e
danielcuque <3024465830102@ingenieria.usac.edu.gt>
2024-02-27 23:59:47 -0600

    feat: get info and update info

--- backend-node/src/auth/auth.controller.ts
+++ backend-node/src/auth/auth.controller.ts
@@ -8,11 +8,17 @@ import {
   ValidationPipe,
   ParseFilePipe,
   FileTypeValidator,
+  Patch,
+  UseGuards,
+  Headers,
+  Get,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { memoryStorage } from 'multer';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { AuthGuard } from './guards/jwt-guard';
 
 @Controller('auth')
 export class AuthController {
@@ -38,6 +44,36 @@ export class AuthController {
       ...createUserDto,
       photo,
     });
+  }
+
+  @UseGuards(AuthGuard)
+  @Patch('info')
+  @UsePipes(new ValidationPipe({ transform: true }))
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: memoryStorage(),
+    }),
+  )
+  updateInfo(
+    @Body() updateUserDto: UpdateUserDto,
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [new FileTypeValidator({ fileType: '.(png|jpeg|jpg)' })],
+      }),
+    )
+    photo: Express.Multer.File,
+    @Headers('authorization') token: string,
+  ) {
+    return this.authService.update({
+      ...updateUserDto,
+      photo,
+    }, token);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('info')
+  getUserInfo(@Headers('authorization') token: string) {
+    return this.authService.getUserInfo(token);
   }
 
   @Post('login')
