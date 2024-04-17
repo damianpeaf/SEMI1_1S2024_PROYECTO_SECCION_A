0489227b40caf77cef1261cbd3544b5bf7119fbf
MedinaJeser <jesermedina570@gmail.com>
2024-04-17 16:56:35 -0600

    [fix]: register & login

--- Proyecto1/backend/routes/auth/register.py
+++ Proyecto1/backend/routes/auth/register.py
@@ -9,6 +9,7 @@ class RegisterData(BaseModel):
     username: str
     password: str
     name: str
+    id: int
 
 
 @router.post("/register", response_model=dict, status_code=200)
@@ -16,7 +17,7 @@ async def register(data: RegisterData):
     # Register user on Cognito
     print("Registering user")
     try:
-        response = register_user(data.username, data.password, data.name)
+        response = register_user(data.username, data.password, data.name, data.id)
         # TODO: Register user on DB
 
     except Exception as e:
