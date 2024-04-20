22329f481547152e21116121c2d913a41ee3f893
danielcuque <3024465830102@ingenieria.usac.edu.gt>
2024-04-19 20:16:24 -0600

    feat(backend): endpoint to delete project

--- Proyecto1/backend/models/project_model.py
+++ Proyecto1/backend/models/project_model.py
@@ -26,7 +26,7 @@ class ProjectModel:
             raise e
         
     @classmethod
-    def get_project_by_id(self, project_id: int):
+    def get_project_by_id(self, project_id: int) -> Project:
         try:
             connection = get_connection()
 
@@ -52,6 +52,26 @@ class ProjectModel:
                 cursor.execute(
                     "UPDATE project SET title = %s, description = %s, location = %s, category = %s WHERE id = %s",
                     (project.title, project.description, project.location, project.category, project.id)
+                )
+                affected_rows = cursor.rowcount
+                connection.commit()
+
+            connection.close()
+            return {"affected_rows": affected_rows}
+
+        except Exception as e:
+            print(e)
+            raise e
+        
+    @classmethod
+    def delete_project(self, project_id: int):
+        try:
+            connection = get_connection()
+
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    "DELETE FROM project WHERE id = %s",
+                    (project_id,)
                 )
                 affected_rows = cursor.rowcount
                 connection.commit()
--- Proyecto1/backend/routes/project/project.py
+++ Proyecto1/backend/routes/project/project.py
@@ -158,3 +158,56 @@ async def update_project(project_id: int, project_data: ProjectData, token: str 
     except Exception as e:
         print(e)
     return JSONResponse({"message": "Error updating project", "status": 500}, 500)
+
+@router.delete("/projects/{project_id}", response_model=dict, status_code=200)
+async def delete_project(project_id: int, token: str = Depends(oauth2_scheme)):
+    try:
+        payload = Security.check_token(token)
+
+        if payload is None:
+            return JSONResponse({"message": "Unauthorized", "status": 401}, 401)
+        
+        user_id = payload["id"]
+        project = ProjectModel.get_project_by_id(project_id)
+        if project is None:
+            return JSONResponse({"message": "Project not found", "status": 404}, 404)
+
+        user_project = UserProjectModel.get_user_project(user_id, project_id)
+
+        if user_project is None:
+            return JSONResponse(
+                {
+                    "error": "Unauthorized",
+                    "status": 401,
+                    "message": "User is not authorized to delete this project",
+                },
+                401,
+            )
+
+        result_db = ProjectModel.delete_project(project_id)
+
+        if result_db["affected_rows"] != 1:
+            return JSONResponse(
+                {
+                    "error": "Error deleting project",
+                    "status": 500,
+                    "message": "Error deleting project on DB",
+                },
+                500,
+            )
+        response = {
+            "message": "Project deleted successfully",
+            "status": 200,
+            "data": {
+                "id": project_id,
+                "name": project.title,
+                "description": project.description,
+                "category": project.category,
+                "location": project.location
+            }
+        }
+        return JSONResponse(response, 200)
+
+    except Exception as e:
+        print(e)
+    return JSONResponse({"message": "Error deleting project", "status": 500}, 500)
